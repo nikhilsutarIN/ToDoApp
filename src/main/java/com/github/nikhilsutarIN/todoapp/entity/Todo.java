0ec0ef837afaae2c41316e61f6da01e4690c8658
iamnikhilsutar <iamnikhilsutar@gmail.com>
2025-12-30 19:31:06 +0530

    Update file

--- src/main/java/com/github/nikhilsutarIN/todoapp/entity/Todo.java
+++ src/main/java/com/github/nikhilsutarIN/todoapp/entity/Todo.java
@@ -1,30 +1,15 @@
 package com.github.nikhilsutarIN.todoapp.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Todo {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
+    private String id;
     private String title;
-
     private String description;
-
     private boolean status;
-
-    @ManyToOne(fetch =  FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    @JsonIgnore
-    private User user;
-
 }
--- src/main/java/com/github/nikhilsutarIN/todoapp/entity/User.java
+++ src/main/java/com/github/nikhilsutarIN/todoapp/entity/User.java
@@ -1,9 +1,10 @@
 package com.github.nikhilsutarIN.todoapp.entity;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,24 +13,19 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-@Entity
+@Document(collection = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "user")
 public class User implements UserDetails {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private String id;
 
     private String firstname;
     private String lastname;
     private String email;
     private String password;
     private String role;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     private List<Todo> todos;
 
     @Override
